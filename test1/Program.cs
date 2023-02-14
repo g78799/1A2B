aa51e10531a299c0c1154387ae80963d7f328517
g87899 <apple8912141@gmail.com>
2023-02-14 15:00:54 +0800

    0000

--- test1/Program.cs
+++ test1/Program.cs
@@ -12,6 +12,29 @@ namespace test1
         {
             Console.WriteLine("歡迎來到1A2B猜數字遊戲");
 
+            int[] ans = new int[4];
+            Random rnd = new Random();
+
+             ans[0] = rnd.Next(0, 10);
+             ans[1] = rnd.Next(0, 10);
+             ans[2] = rnd.Next(0, 10);
+             ans[3] = rnd.Next(0, 10);
+
+            for(int i = 0;i<4;i++)
+            {
+               for(int j=0;j<i;j++)
+                {
+                    while (ans[j] == ans[i])
+                    {
+                        ans[j]=rnd.Next(10);
+                    }
+                }
+            }
+            Console.WriteLine($"{ans[0]},{ans[1]},{ans[2]},{ans[3]}");
+            Console.ReadLine();
+
+
+
         }
     }
 }
