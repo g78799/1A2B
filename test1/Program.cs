f225140da18f2cd8774a92ee795fdbb55dce1a91
g87899 <apple8912141@gmail.com>
2023-02-14 11:36:05 +0800

    加入專案檔案。

---
+++ test1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("歡迎來到1A2B猜數字遊戲");
+
+        }
+    }
+}
