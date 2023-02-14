69dbfdc7e03c4f433bcdb252605907d3c3d35420
g87899 <apple8912141@gmail.com>
2023-02-14 16:38:45 +0800

    01

--- test1/Program.cs
+++ test1/Program.cs
@@ -12,26 +12,73 @@ namespace test1
         {
             Console.WriteLine("歡迎來到1A2B猜數字遊戲");
 
-            int[] ans = new int[4];
+            int[] ints = new int []{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            int tp = 0;
             Random rnd = new Random();
 
-             ans[0] = rnd.Next(0, 10);
-             ans[1] = rnd.Next(0, 10);
-             ans[2] = rnd.Next(0, 10);
-             ans[3] = rnd.Next(0, 10);
-
-            for(int i = 0;i<4;i++)
+            for (int i = 0; i<ints.Length; i++)
             {
-               for(int j=0;j<i;j++)
+                int n = rnd.Next(ints.Length); 
+                tp = ints[i];
+                ints[i] = ints[n];
+                ints[n] = tp;
+            }
+
+
+
+            Console.WriteLine($"{ints[0]},{ints[1]},{ints[2]},{ints[3]}");
+            Console.WriteLine("請輸入數字");
+
+            int[] enter = new int[4];
+            int a = 0, b = 0;
+            string c = Console.ReadLine();
+
+            
+                for (int i = 0; i < 4; i++)
                 {
-                    while (ans[j] == ans[i])
+                    enter[i] = Convert.ToInt32(c[i].ToString());
+                }
+
+
+
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (enter[i] == ints[i])
                     {
-                        ans[j]=rnd.Next(10);
+                        a++;
+                    }
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (enter[j] == ints[i])
+                        {
+                          b++;
+                        }
                     }
                 }
+
+
+                b -= a;
+
+                
+
+                Console.WriteLine($"{a}A,{b}B");
+            
+
+            
+
+            if (a==4)
+            {
+                Console.WriteLine("答案正確");
             }
-            Console.WriteLine($"{ans[0]},{ans[1]},{ans[2]},{ans[3]}");
+
+            
+
+            
+
+
             Console.ReadLine();
+
 
 
 
